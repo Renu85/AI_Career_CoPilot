050e5d9ed77ffc7a709a780155f96a23dd28e930
Renu Shokeen <renushokeen85@gmail.com>
2026-06-04 03:47:18 +0530

    AI_Career_CoPilot

--- db.py
+++ db.py
@@ -6,12 +6,13 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
+load_dotenv(dotenv_path=".env", override=True)
 
 
 DATABASE_URL=os.getenv("DATABASE_URL") 
 
-print("DATABASE_URL =",DATABASE_URL)
+print("ENV loaded:", load_dotenv(".env"))
+print("DATABASE_URL:", repr("DATABASE_URL"))
 
 engine  = create_engine(
      DATABASE_URL,
